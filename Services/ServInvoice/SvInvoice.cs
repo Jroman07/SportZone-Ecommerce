ceb60c5370efae58a617fbdc4269dda059a7224c
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-06-05 11:13:35 -0600

    first commit

---
+++ Services/ServInvoice/SvInvoice.cs
@@ -0,0 +1,100 @@
+﻿using Entidades;
+using Microsoft.EntityFrameworkCore;
+using Services.Customer;
+using Services.MyDbContext;
+using Services.NewFolder;
+using Services.ServPurchaseDetail;
+
+
+namespace Services.Invoice
+{
+    public class SvInvoice : ISvInvoice
+    {
+        private MyContext _myDbContext = default!;
+        private SvCustomer _svCustomer = default!;
+        private SvPurchaseDatail _svPurchaseDetail = default!;
+        public SvInvoice()
+        {
+            _myDbContext = new MyContext();
+            _svCustomer = new SvCustomer();
+            _svPurchaseDetail = new SvPurchaseDatail();
+        }
+        public Entidades.Invoice AddInvoice(Entidades.Invoice newInvoice)
+        {
+            Entidades.Customer custumer = _svCustomer.GetCostumerById(newInvoice.CostumerId);
+            if (custumer == null)
+            {
+                return newInvoice = null;
+            }
+            else
+            {
+                List<PurchaseDetail> purchaseDetails = _svPurchaseDetail.GetAllPurchasesByCustumerId(newInvoice.CostumerId);
+                if (!purchaseDetails.Any())
+                {
+                    return newInvoice = null;
+                }
+                else
+                {
+                    newInvoice.Sets(purchaseDetails);
+                    _myDbContext.Invoices.Add(newInvoice);
+                    _myDbContext.SaveChanges();
+                    InvoiceMail(newInvoice, purchaseDetails);
+                    _svPurchaseDetail.DeletePurchasesByCustumerId(purchaseDetails);
+                    return newInvoice;
+                }
+            }
+        }
+
+        public List<Entidades.Invoice> GetAllInvoices()
+        {
+            return _myDbContext.Invoices.Include(x => x.Customer).ToList();
+        }
+
+        public Entidades.Invoice GetInvoiceById(int invoiceId)
+        {
+            return _myDbContext.Invoices.Include(x => x.Customer).SingleOrDefault(x => x.Id == invoiceId);
+        }
+
+        void InvoiceMail(Entidades.Invoice invoice, List<PurchaseDetail> purchaseDetails_Customer)
+        {
+            // Obtener información del cliente
+            Entidades.Customer customer = _svCustomer.GetCostumerById(invoice.CostumerId);
+
+            // Enviar correo electrónico
+            Mail mail = new Mail();
+            string body = mail.GenerateHtml(purchaseDetails_Customer, invoice, customer);
+            mail.sendMail(customer.Email, "Factura SportZone", body);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//objLogic.sendMail("samuelbarrantes1@gmail.com", "Este correo fue enviado via C-sharp", body);
---
+++ Services/ServPurchaseDetail/SvPurchaseDatail.cs
@@ -0,0 +1,80 @@
+﻿using Entidades;
+using Microsoft.EntityFrameworkCore;
+using Services.Customer;
+using Services.MyDbContext;
+using Services.Shoe;
+
+namespace Services.ServPurchaseDetail
+{
+    public class SvPurchaseDatail : ISvPurchaseDatail
+    {
+        private MyContext _myDbContext = default!;
+        private SvShoe _svShoe = default!;
+        public SvPurchaseDatail()
+        {
+            _myDbContext = new MyContext();
+            _svShoe = new SvShoe();
+        }
+        public PurchaseDetail AddPurchase(PurchaseDetail purchase)
+        {
+            Entidades.Shoe shoe = _svShoe.GetShoeById(purchase.ShoeId);
+            if (shoe.SubtractStock(purchase.Quantity) == false)
+            {
+                return purchase = null;
+            }
+            else
+            {
+                _svShoe.UpdateStock(purchase.ShoeId, shoe);
+                purchase.setPrice(shoe);
+                _myDbContext.PurchaseDetails.Add(purchase);
+                _myDbContext.SaveChanges();
+
+                return purchase;
+            }
+        }
+
+        public void DeletePurchase(int id)
+        {
+            PurchaseDetail deletePurchase = _myDbContext.PurchaseDetails.Find(id);
+            if (deletePurchase != null)
+            {
+                _myDbContext.PurchaseDetails.Remove(deletePurchase);
+                _myDbContext.SaveChanges();
+            }
+        }
+
+        public List<PurchaseDetail> GetAllPurchases()
+        {
+            return _myDbContext.PurchaseDetails.Include(x => x.Shoe).Include(x => x.Customer).ToList();
+        }
+
+        public PurchaseDetail GetPurchaseById(int id)
+        {
+            return _myDbContext.PurchaseDetails.Include(x => x.Shoe).Include(x => x.Customer).SingleOrDefault(x => x.Id == id);
+        }
+
+        public PurchaseDetail UpdatePurchase(int id, PurchaseDetail purchase)
+        {
+            PurchaseDetail purchaseUpdate = _myDbContext.PurchaseDetails.Find(id);
+            purchaseUpdate.Quantity = purchase.Quantity;
+            purchaseUpdate.ShoeId = purchase.ShoeId;
+
+            _myDbContext.Update(purchaseUpdate);
+            _myDbContext.SaveChanges();
+
+            return purchaseUpdate;
+        }
+
+        public List<PurchaseDetail> GetAllPurchasesByCustumerId(int IdCustumer)
+        {
+            return _myDbContext.PurchaseDetails.Where(x => x.CustomerId == IdCustumer).ToList();
+            //_myDbContext.PurchaseDetails.RemoveRange();
+        }
+
+        public void DeletePurchasesByCustumerId(List<PurchaseDetail> purchaseDetailsCustomer)
+        {
+            _myDbContext.PurchaseDetails.RemoveRange(purchaseDetailsCustomer);
+            _myDbContext.SaveChanges();
+        }
+    }
+}
